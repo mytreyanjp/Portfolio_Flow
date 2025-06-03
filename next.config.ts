2e0f25c26147a6059c694d11f659d565cfcec8fa
Mytreyan JP <mytreyan197@gmail.com>
2025-06-03 12:11:53 +0000

    cloud hosting log?

--- next.config.ts
+++ next.config.ts
@@ -3,22 +3,22 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // typescript: {
-  //   ignoreBuildErrors: true, // Temporarily removed to surface all errors
-  // },
-  // eslint: {
-  //   ignoreDuringBuilds: true, // Temporarily removed to surface all errors
-  // },
-  // images: { // Temporarily commented out to simplify config
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'placehold.co',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //   ],
-  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
